1f99b54c205e0a1f9a8e378ec3e48862ac93f72d
Wendell Costa <wendellcosta9141@gmail.com>
2021-02-26 20:02:47 -0300

    Exercicio 6 resolvido

---
+++ bloco_4/dia_2/Dia_4.2_Exercicio_6.js
@@ -0,0 +1,16 @@
+//Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numerosImpar = 0;
+
+for (var index = 0; index < numbers.length; index += 1) {
+   if (numbers[index] % 2 != 0) {
+      numerosImpar += 1;
+   }
+}
+
+if(numerosImpar === 0) {
+   console.log('nenhum valor ímpar encontrado');
+} else {
+   console.log('numeros ímpar: ' +  numerosImpar);
+}
--- bloco_4/dia_2/Dia_4.2_Exercicio_7.js
+++ bloco_4/dia_2/Dia_4.2_Exercicio_7.js
@@ -1,12 +1,12 @@
 //Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 1, 35, 27];
-let maiorNumero = numbers[0];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menorNumero = numbers[0];
 
 for (var index = 0; index < numbers.length; index++) {
-   if (numbers[index] < maiorNumero) {
-      maiorNumero = numbers[index];
+   if (numbers[index] < menorNumero) {
+      menorNumero = numbers[index];
    }
 }
 
-console.log(maiorNumero)
+console.log(menorNumero)
