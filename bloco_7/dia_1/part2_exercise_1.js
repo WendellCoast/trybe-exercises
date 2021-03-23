c3a5d4dbbde1f09485dc3dc70713390fba1bfc6b
Wendell Costa <wendellcosta9141@gmail.com>
2021-03-23 19:19:34 -0300

    [Exercicio 1/Parte 2 - Concluido]

---
+++ bloco_7/dia_1/part2_exercise_1.js
@@ -0,0 +1,11 @@
+const factorial = number => {
+    let result = 1
+
+    for (let index = 2; index <= number; index += 1) {
+        result *= i
+    }
+
+    return result
+}
+
+console.log(factorial(5))
